b5f6912d6f824213f3179720588dff1964fdd58b
jackie <515878133@qq.com>
2021-04-24 21:23:45 +0800

    [Chapter_1/1-13] rename and complete printHistogram.c 100%

--- Chapter_1/1-13/printHistogram.c
+++ Chapter_1/1-13/printHistogram.c
@@ -2,16 +2,9 @@
 #include <stdlib.h>
 
 #define MAX         100
-#define MAXLENGTH   50
-#define HORIZENTAL  0
-#define VERTICAL    1
 
 /* Write a program to print a histogram of the lengths of words in its 
- * input. Vertical version */
-/* TODO: 
- * 1 程序有些臃肿, 需要精简   <-- OK
- * 2 当单词长度较大时，无法整齐的打印出来  <-- read printf in <the C programming language>
- * 3 用一个具有多行数据的文件，用重定向输入程序，会出现core dump的问题 */
+ * input, horizontally and vertically */
 
 int readwords(char *wordsptr[], int *length, int maxwords);
 int getMaxElem(int a[], int length);
@@ -55,7 +48,7 @@ int readwords(char *wordsptr[], int *length, int maxwords)
     while ((c = getchar()) != EOF) {
         words_length = 0;
         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-            p = (char *)malloc(MAXLENGTH);
+            p = (char *)malloc(MAX);
             *(p + words_length) = c;
             words_length++;
             while (((c = getchar()) >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
@@ -63,6 +56,9 @@ int readwords(char *wordsptr[], int *length, int maxwords)
                 words_length++;
             }
         }
+        else
+            continue;
+
         *(wordsptr + words_count) = p;
         length[words_count] = words_length;
 
@@ -71,6 +67,36 @@ int readwords(char *wordsptr[], int *length, int maxwords)
     }
 
     return words_count;
+}
+
+/* printHistogram: print horizontal and vertical histogram */
+void printHistogram(char *wordsptr[], int *length, int words_count)
+{
+    int i, j, max_words_length;
+    char horizontal_matrix[MAX][MAX] = {0};
+    char vertical_matrix[MAX][MAX] = {0};
+    char symbol;
+
+    symbol = '|';
+
+    max_words_length = getMaxElem(length, words_count);
+
+    for (i = 0; i < words_count; i++)
+        for (j = 0; j < max_words_length; j++)
+            if (j < length[i])
+                horizontal_matrix[i][j] = '|';
+            else 
+                horizontal_matrix[i][j] = ' ';
+
+    matrixMagic(vertical_matrix, horizontal_matrix, words_count, max_words_length);
+
+    /* horizontal histogram */
+    printf("\nsource matrix:\n");
+    printHorizontalSymbol(horizontal_matrix, wordsptr, words_count, max_words_length);
+
+    /* vertical histogram */
+    printf("\nDestination matrix:\n");
+    printVerticalSymbol(vertical_matrix, wordsptr, max_words_length, words_count);
 }
 
 /* getMaxElem: find max element in array a */
@@ -97,51 +123,15 @@ void matrixMagic(char (*at)[MAX], char (*a)[MAX], int row, int col)
             at[col-j-1][i] = a[i][j];
 }
 
-/* printHistogram: print horizontal and vertical histogram */
-void printHistogram(char *wordsptr[], int *length, int words_count)
-{
-    int i, j, max_words_length;
-    char horizontal_matrix[MAX][MAX] = {0};
-    char vertical_matrix[MAX][MAX] = {0};
-    char symbol;
-
-    symbol = '|';
-
-    max_words_length = getMaxElem(length, words_count);
-
-    for (i = 0; i < words_count; i++)
-        for (j = 0; j < max_words_length; j++)
-            if (j < length[i])
-                horizontal_matrix[i][j] = '|';
-            else 
-                horizontal_matrix[i][j] = ' ';
-
-    matrixMagic(vertical_matrix, horizontal_matrix, words_count, max_words_length);
-
-    /* horizontal histogram */
-    /* print matrix */
-    printf("\nsource matrix:\n");
-
-    /* print symbol */
-    printHorizontalSymbol(horizontal_matrix, wordsptr, words_count, max_words_length);
-
-    /* vertical histogram */
-    /* print matrix */
-    printf("\nDestination matrix:\n");
-
-    /* print symbol */
-    printVerticalSymbol(vertical_matrix, wordsptr, max_words_length, words_count);
-}
-
 /* printHorizontalSymbol: print horizontal symbol */
 void printHorizontalSymbol(char (*matrix)[MAX], char *wordsptr[], int row, int col)
 {
     int i, j;
 
     for (i = 0; i < row; i++) {
-        printf("%s\t\t", *(wordsptr + i));
+        printf("%12s:  ", *(wordsptr + i));
         for (j = 0; j < col; j++)
-            printf("%c  ", matrix[i][j]);
+            printf("%-2c", matrix[i][j]);
         printf("\n");
     }
 }
@@ -153,15 +143,12 @@ void printVerticalSymbol(char (*matrix)[MAX], char *wordsptr[], int row, int col
 
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++)
-            if (matrix[i][j] == ' ')
-                printf("\t\t");
-            else
-                printf("%c\t\t", matrix[i][j]);
+            printf("%-12c", matrix[i][j]);
         printf("\n");
     }
 
     for (i = 0; i < col; i++)
-        printf("%s\t\t", *(wordsptr + i));
+        printf("%-12s", *(wordsptr + i));
 
     putchar('\n');
 }
