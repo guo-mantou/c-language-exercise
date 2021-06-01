013a855215ce7064e47853330fe3eafd8d2f8791
jackie <515878133@qq.com>
2021-06-01 23:36:37 +0800

    [Chapter_2/2-1] 75%

--- Chapter_2/2-1/2-1.c
+++ Chapter_2/2-1/2-1.c
@@ -19,15 +19,17 @@
 #define MYSCHAR_MAX     (char)cal2ex_int(7) - 1
 #define MYSCHAR_MIN     (char)-cal2ex_int(7)
 #define MYUCHAR_MAX     (unsigned char)cal2ex_int(8) - 1
-#define MYSHRT_MAX		(short)cal2ex_int(15) - 1
-#define MYSHRT_MIN		(short)-cal2ex_int(15)
-#define MYUSHRT_MAX		(unsigned short)cal2ex_int(16) - 1
-#define MYINT_MAX		(int)cal2ex_int(31) - 1
-#define MYINT_MIN		(int)-cal2ex_int(31)
-#define MYUINT_MAX		(unsigned int)cal2ex_int(32)
-#define MYLONG_MAX		(long)cal2ex_int(63) - 1
-#define MYLONG_MIN		(long)-cal2ex_int(63)
-#define MYULONG_MAX		cal2ex_int(64) - 1
+#define MYSHRT_MAX      (short)cal2ex_int(15) - 1
+#define MYSHRT_MIN      (short)-cal2ex_int(15)
+#define MYUSHRT_MAX     (unsigned short)cal2ex_int(16) - 1
+#define MYINT_MAX       (int)cal2ex_int(31) - 1
+#define MYINT_MIN       (int)-cal2ex_int(31)
+#define MYUINT_MAX      (unsigned int)cal2ex_int(32)
+#define MYLONG_MAX      (long)cal2ex_int(63) - 1
+#define MYLONG_MIN      (long)-cal2ex_int(63)
+#define MYULONG_MAX     (unsigned long)cal2ex_int(64) - 1
+#define MYFLT_MAX       (cal2ex_int(24)-1)*cal2ex_int(104)
+#define MYFLT_MIN       -((cal2ex_int(24)-1)*cal2ex_int(104))
 
 #define PRINT_VALUE(var1, conver_char, var2, var3)  printf("%-16s%50"#conver_char"%50"#conver_char"\n", var1, var2, var3)
 #define PRINT_CHAR(max, min)           PRINT_VALUE(CHAR, hhd, max, min)
@@ -40,7 +42,7 @@
 #define PRINT_UNSIGNED_LONG(max, min)  PRINT_VALUE(ULONG, lu, max, min)
 #define PRINT_FLOAT(max, min)          PRINT_VALUE(FLOAT, f, max, min)
 #define PRINT_HEADER(header_1, header_2, header_3) PRINT_VALUE(header_1, s, header_2, header_3)
-/* TODO: cal float range */
+/* TODO: float minimum value of standard is different from the calculate one, why? */
 
 /* Exercise 2-1. Write a program to determine the ranges of char, short, int,
  * and long variables, both signed and unsigned, by printing appropriate 
@@ -51,7 +53,7 @@ void print_range_standard_header();
 void print_range_direct_compute();
 void print_dashes(int len);
 void print_header();
-unsigned long cal2ex_int(int ex);
+float cal2ex_int(int ex);
 
 enum choice {STDHEADERS=1, DIRCOMPUT, EXIT=0};
 
@@ -136,6 +138,7 @@ void print_range_direct_compute()
     PRINT_UNSIGNED_LONG(MYULONG_MAX, (unsigned long)0);
     printf("\n");
 
+    PRINT_FLOAT(MYFLT_MAX, MYFLT_MIN);
     print_dashes(DASHLENGTH);
     printf("\n");
 }
@@ -155,9 +158,10 @@ void print_header()
 }
 
 /* calculate exponet of 2 */
-unsigned long cal2ex_int(int ex)
+float cal2ex_int(int ex)
 {
-    unsigned long i, num;
+    unsigned long i;
+    float num;
 
     for (i=0, num=1; i < ex; i++)
         num = num * 2;
