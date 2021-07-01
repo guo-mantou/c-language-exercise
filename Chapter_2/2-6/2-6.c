8a81d1b35357986caf3ab2ebd2760d691fa9cd25
jackie <515878133@qq.com>
2021-07-01 23:29:25 +0800

    [Chapter_2/2-6] Update

--- Chapter_2/2-6/2-6.c
+++ Chapter_2/2-6/2-6.c
@@ -5,22 +5,23 @@
  * bits that begin at position p set to the rightmost n bits of y, leaving the 
  * other bits unchanged. */
 
-int setbits(int x, int p, int n, int y);
-int getbits(int x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+unsigned getbits(unsigned x, int p, int n);
 
 int main(int argc, char *argv[])
 {
-    int x, p, n, y;
+    unsigned x, y;
+    int p, n;
 
     if (argc != 5) {
         printf("Usage: %s x p n y\n", argv[0]);
         return -1;
     }
 
-    x = (int)strtol(argv[1], NULL, 16);
+    x = (unsigned)strtol(argv[1], NULL, 16);
     p = (int)strtol(argv[2], NULL, 10);
     n = (int)strtol(argv[3], NULL, 10);
-    y = (int)strtol(argv[4], NULL, 16);
+    y = (unsigned)strtol(argv[4], NULL, 16);
 
     printf("0x%x\n", setbits(x, p, n, y));
 
@@ -29,7 +30,7 @@ int main(int argc, char *argv[])
 
 /* setbits:  return x with the n bits that begin at position p set to the rightmost
  * n bits of y, leaving the other bits unchanged */
-int setbits(int x, int p, int n, int y)
+unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
     int z;
 
@@ -38,12 +39,13 @@ int setbits(int x, int p, int n, int y)
     y = getbits(y, n-1, n);
     y = (y << (p-n+1)) | ~(~0 << (p-n+1));
 
-    x = x | (~(~0 << n) << (p-n+1));
+    x = x | (~(~0 << n) << (p-n+1));   /* n is 2, x: 01100010 \
+                                          -> 01110010 */
     return x & y | z;
 }
 
 /* getbits:  get n bits from position p */
-int getbits(int x, int p, int n)
+unsigned getbits(unsigned x, int p, int n)
 {
     return (x >> (p+1-n)) & ~(~0 << n);
 }
