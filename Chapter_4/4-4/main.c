0ba5a9762bc23d70baefcfa1ceb3bbe0732b14ec
guosj.fnst <guosj.fnst@fujitsu.com>
2021-11-15 17:41:19 +0800

    [Chapter_4] update 4-4, 50%

--- Chapter_4/4-4/main.c
+++ Chapter_4/4-4/main.c
@@ -3,37 +3,42 @@
 #include "calc.h"
 
 #define MAXOP    100 /* max size of operand or operator */
+#define tpush(x)    push(top_elem = (x))  /* make sure top_elem always right */
+
+/* Exercise 4-4. Add commands to print the top element of the stack without pop-
+ * ping, to duplicate it, and to swap the top two elements. Add a command to
+ * clear the stack */
 
 /* reverse Polish calculator */
 int main(void)
 {
     int type;
-    double op2;
+    double op2, top_elem;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF) {
         switch (type) {
             case NUMBER:
-                push(atof(s));
+                tpush(atof(s));
                 break;
 
             case '+':
-                push(pop() + pop());
+                tpush(pop() + pop());
                 break;
 
             case '*':
-                push(pop() * pop());
+                tpush(pop() * pop());
                 break;
 
             case '-':
                 op2 = pop();
-                push(pop() - op2);
+                tpush(pop() - op2);
                 break;
 
             case '/':
                 op2 = pop();
                 if (op2 != 0.0)
-                    push(pop() / op2);
+                    tpush(pop() / op2);
                 else
                     printf("error: zero divisor\n");
                 break;
@@ -41,13 +46,35 @@ int main(void)
             case '%':
                 op2 = pop();
                 if (op2 != 0.0)
-                    push((int)pop() % (int)op2);
+                    tpush((int)pop() % (int)op2);
                 else
                     printf("error: zero divisor\n");
                 break;
 
             case '\n':
                 printf("\t%.8g\n", pop());
+                break;
+
+                /* FIXME: input 3 4 + 5 6 * p s p -> '\n' -> p Ctrl-d
+                 * print still 7, which should be updated to 30! */
+            case 'p':    /* print top element of the stack */
+                printf("\n\ttop element of the stack: %.8g\n", top_elem);
+                break;
+
+            case 'd':    /* duplicate top element */
+                tpush(top_elem);
+                printf("\n\ttop element has been duplicated\n");
+                break;
+
+            case 's':    /* swap the top two elements */
+                {
+                    double elem1, elem2;
+                    elem1 = pop();
+                    elem2 = pop();
+                    tpush(elem1);
+                    tpush(elem2);
+                    printf("\n\tthe top two elements have been swapped\n");
+                }
                 break;
 
             default:
