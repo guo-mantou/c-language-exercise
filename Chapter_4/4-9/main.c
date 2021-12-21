fc7887fce9978e4baf48db24a2882b727b80afb1
guosj.fnst <guosj.fnst@fujitsu.com>
2021-12-21 17:21:25 +0800

    [Chapter_4] update exercise 4-9

--- Chapter_4/4-9/main.c
+++ Chapter_4/4-9/main.c
@@ -6,8 +6,9 @@
 
 #define MAXOP    100 /* max size of operand or operator */
 
-/* Exercise 4-8. Suppose that there will never be more than one character of
- * pushback. Modify getch and ungetch accordingly. */
+/* Exercise 4-9. Our getch and ungetch do not handle a pushed-back EOF
+ * correctly. Decide what their properties ought to be if an EOF is pushed back,
+ * then implement your design. */
 
 void helpinfo();
 
