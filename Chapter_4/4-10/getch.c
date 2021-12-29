8302e1820fd6e68bb88e805f4faef4ede913254c
guosj.fnst <guosj.fnst@fujitsu.com>
2021-12-29 11:03:16 +0800

    [Chapter_4] update exercise 4-10, tomorrow check answer

--- Chapter_4/4-10/getch.c
+++ Chapter_4/4-10/getch.c
@@ -2,13 +2,44 @@
 #include "calc.h"
 #define BUFSIZE  100
 
-int buf[BUFSIZE];    /* buffer for ungetch */
+char buf[BUFSIZE];    /* buffer for ungetch */
 int bufp = 0;         /* next free position in buf */
+
+/* mygetline:  read an entire input line from standard input, return length */
+static int mygetline(char s[], int max)
+{
+    int c, i;
+
+    for (i = 0; i < max-1 && (c=getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    if (c == '\n')
+        s[i++] = c;
+    if (i == 0 && c == EOF)
+        s[i++] = EOF;
+    s[i] = '\0';
+    return i;
+}
+
+/* reverse:  reverse string s */
+static void reverse(char s[], int len)
+{
+    int i, c;
+
+    for (i = 0; i < len/2; i++) {
+        c = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = c;
+    }
+}
 
 /* getch:  get a (possibly pushed-back) character */
 int getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    if (bufp <= 0) {
+        bufp = mygetline(buf, BUFSIZE);
+        reverse(buf, bufp);
+    }
+    return buf[--bufp];
 }
 
 /* ungetch:  push character back on input */
--- Chapter_4/4-10/getop.c
+++ Chapter_4/4-10/getop.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "calc.h"
+#define BUFSIZE  100
+
+static char buf[BUFSIZE];    /* buffer for ungetch */
+static int bufp = 0;         /* next free position in buf */
+
+/* mygetline:  read an entire input line from standard input, return length */
+static int mygetline(char s[], int max)
+{
+    int c, i;
+
+    for (i = 0; i < max-1 && (c=getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    if (c == '\n')
+        s[i++] = c;
+    if (i == 0 && c == EOF)
+        s[i++] = EOF;
+    s[i] = '\0';
+    return i;
+}
+
+/* reverse:  reverse string s */
+static void reverse(char s[], int len)
+{
+    int i, c;
+
+    for (i = 0; i < len/2; i++) {
+        c = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = c;
+    }
+}
 
 /* getop:  get next character or numeric operand */
 int getop(char s[])
 {
     int i, c;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
+    /* get another line when buffer is empty */
+    if (bufp <= 0) {
+        bufp = mygetline(buf, BUFSIZE);
+        reverse(buf, bufp);
+    }
+
+    while ((s[0] = c = buf[--bufp]) == ' ' || c == '\t')
         ;
     s[1] = '\0';
     if (!isdigit(c) && c != '.' && c != '+'
             && c != '-')
         return c;      /* not a number */
     if (c == '+' || c == '-') {
-        if (!isdigit(s[1] = c = getch())) {
-            ungetch(c);
+        if (!isdigit(s[1] = c = buf[--bufp])) {
+            ++bufp;
             s[1] = '\0';
             return s[0];
         } else
@@ -23,14 +60,12 @@ int getop(char s[])
     } else
         i = 0;
     if (isdigit(c))    /* collect integer part */
-        while (isdigit(s[++i] = c = getch()))
+        while (isdigit(s[++i] = c = buf[--bufp]))
             ;
     if (c == '.')      /* collect fraction part */
-        while (isdigit(s[++i] = c = getch()))
+        while (isdigit(s[++i] = c = buf[--bufp]))
             ;
     s[i] = '\0';
-    if (c == EOF)
-        printf("getop:  Got EOF\n");
-    ungetch(c);
+    ++bufp;
     return NUMBER;
 }
